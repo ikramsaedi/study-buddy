5ba43453e170611647bc1197834ba2894461dbcb
Ikram Saedi <ikramsaedi@gmail.com>
2024-09-20 23:39:30 +1000

    Use bottom tab navigation instead

--- App.tsx
+++ App.tsx
@@ -1,57 +1,30 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { Button, StyleSheet, Text, View } from "react-native";
+import { Text } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { Leaderboard } from "./components/Leaderboard";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
+      <Tab.Navigator
+        sceneContainerStyle={{ backgroundColor: "#E0C7E9" }}
+        screenOptions={{}}
+      >
+        <Tab.Screen
           name="Home"
-          component={HomeScreen}
+          component={Leaderboard}
           options={{ title: "Welcome" }}
         />
-        <Stack.Screen name="Profile" component={ProfileScreen} />
-      </Stack.Navigator>
+        <Tab.Screen name="Profile" component={ProfileScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
   );
 }
 
-const Idk = () => (
-  <View style={styles.container}>
-    <Text>Open up App.tsx to start working on your app!</Text>
-    <StatusBar style="auto" />
-  </View>
-);
-
-const Loaf = () => (
-  <View style={styles.container}>
-    <Text>LOAF</Text>
-    <StatusBar style="auto" />
-  </View>
-);
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-});
-
-const HomeScreen = ({ navigation }) => {
-  return (
-    <Button
-      title="Go to Jane's profile"
-      onPress={() => navigation.navigate("Profile", { name: "Jane" })}
-    />
-  );
-};
 const ProfileScreen = ({ navigation, route }) => {
-  return <Text>This is {route.params.name}'s profile</Text>;
+  return <Text>This is Ikram's profile</Text>;
 };
---
+++ components/Leaderboard.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#E0C7E9",
+  },
+});
+export function Leaderboard() {
+  return (
+    <View style={styles.container}>
+      <Text>Loaf</Text>
+    </View>
+  );
+}
